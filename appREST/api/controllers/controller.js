84ae5ca96b5b9b1e38f1f95772681ec5ca94674a
henry <hmsuntaxi@espe.edu.ec>
2019-07-11 20:55:52 -0500

    marca

--- appREST/api/controllers/controller.js
+++ appREST/api/controllers/controller.js
@@ -3,3 +3,46 @@ exports.holaMundo = function(req, res) {
     
     res.json("Hola Mundo");
 };
+exports.crearMarca=function(req,res)
+{
+    var newMarca = new Marca(req.body);
+    newMarca.save(function(err, marca) {
+      if (err)
+          res.send(err);
+      else{
+          res.json(marca);
+      }
+    });
+
+}
+
+exports.findAll = (req, res) => {
+    Marca.find()
+    .then(marcas => {
+        res.send(marcas);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "No existe marca"
+        });
+    });
+};
+exports.findOne = (req, res) => {
+    Marca.findById(req.params.marcaId)
+    .then(marca => {
+        if(!marca) {
+            return res.status(404).send({
+                message: "Marca not found with id " + req.params.marcaId
+            });            
+        }
+        res.send(marca);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Marca not found with id " + req.params.marcaId
+            });                
+        }
+        return res.status(500).send({
+            message: "Something wrong retrieving Marca with id " + req.params.marcaId
+        });
+    });
+};
--- appREST/api/routes/routes.js
+++ appREST/api/routes/routes.js
@@ -3,6 +3,12 @@
 module.exports = function(app) {
     var controller = require('../controllers/controller');
     
-    app.route('/holaMundo')
+   app.route('/holaMundo')
         .get(controller.holaMundo);
+   app.route('/crearMarca')
+        .post(controller.crearMarca);
+   app.route('/listMarca')
+        .get(controller.findAll);
+app.route('/getMarca')
+        .get(controller.findOne);
 }
